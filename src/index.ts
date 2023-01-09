df3dcdbd3df1326371efca154274736f49a2a0c3
Yuliya_Shulgan <juliette7373@gmail.com>
2023-01-09 21:10:33 +0100

    fix: correct stop server

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import { PORT, startServer } from "./server/server.js";
 
 const port = PORT();
 startServer(port);
+process.on('SIGINT', () => {
+    console.log('Gracefully shutting down from SIGINT (Ctrl-C)');
+    process.exit();
+})
