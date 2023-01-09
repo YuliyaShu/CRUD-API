4dbb95b0c24d3d596951da75ec89c9591dd05955
Yuliya_Shulgan <juliette7373@gmail.com>
2023-01-09 20:18:21 +0100

    fix: correct method isRequestBodyValid

--- src/utils/isRequestBodyValid.ts
+++ src/utils/isRequestBodyValid.ts
@@ -3,18 +3,32 @@ import { IncomingMessage, ServerResponse } from "http";
 import { sendResponse } from "./sendResponse.js";
 import { StatusCodes, StatusMessages } from "../server/consts.js";
 
+const checkTypeOfHobbies = (arrayOfHobbies: string[] | []) => {
+    if (arrayOfHobbies.length) {
+        for (const hobby of arrayOfHobbies) {
+            if (typeof hobby !== 'string') return false;
+        }
+    return true;
+    }
+}   
+
 export const isRequestBodyValid = (res: ServerResponse<IncomingMessage>, userData: RequestBody) => {
     try {
         if (!userData) return false;
-        return 'age' in userData 
+        return Object.keys(userData).length === 3
+            && 'age' in userData 
             && typeof userData.age === 'number'
             && 'hobbies' in userData
             && Array.isArray(userData.hobbies)
+            && checkTypeOfHobbies(userData.hobbies)
             && 'username' in userData
-            && typeof userData.username === 'string';
+            && typeof userData.username === 'string'
+            && userData
     } catch (error) {
         console.log('Something went wrong. Try one more time');
         console.error(error);
         sendResponse(res, StatusCodes.INTERNAL_SERVER, StatusMessages.INTERNAL_SERVER);
     }
+        
+        
 }
