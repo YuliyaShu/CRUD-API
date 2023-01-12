09ab8a1ef8a73c9d3dce847e4a88d7767ddf9394
Yuliya_Shulgan <juliette7373@gmail.com>
2023-01-12 13:58:54 +0100

    fix: correct console log

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -5,7 +5,7 @@ import { createBuffer } from './createBuffer.js';
 export const sendResponse = (res: ServerResponse, code: number, message: string, body?: Buffer): void => {
     try {
         const errorResponse = createBuffer({ code: code, message: message }, res)
-        console.log(code);
+        console.log(`Response code: ${code}`);
         res.statusCode = code;
         res.statusMessage = message;
         res.write(body || errorResponse);
